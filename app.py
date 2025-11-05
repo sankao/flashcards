83088fb6f82476894414bc824437cbd403668413
cyrille <cyrille@celerity-trading.com>
2025-11-05 22:39:05 +0800

    fixed ssl loading

--- app.py
+++ app.py
@@ -291,4 +291,4 @@ def clear_all_flashcards():
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
